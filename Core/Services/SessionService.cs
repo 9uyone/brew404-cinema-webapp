8ad841c1f9093d03230d9263076303761c9df2e0
“Kolia” <kozlovskyy.m@nltu.lviv.ua>
2025-01-27 22:30:05 +0200

    Services and DTOS(1)

---
+++ Core/Services/SessionService.cs
@@ -0,0 +1,35 @@
+﻿using DataAccess.EntityModels;
+using DataAccess.Interfaces;
+
+namespace BusinessLogic.Services
+{
+	public class SessionService
+	{
+		private readonly IRepository<Session> _sessionRepository;
+
+		public SessionService(IRepository<Session> sessionRepository)
+		{
+			_sessionRepository = sessionRepository;
+		}
+
+		public async Task<IEnumerable<Session>> GetAllGenresAsync()
+		{
+			return await Task.Run(() => _sessionRepository.Get(includeProperties: "Movie,Hall"));
+		}
+
+		public async Task AddSessionAsync(Session session)
+		{
+			await _sessionRepository.Insert(session);
+		}
+
+		public async Task UpdateSessionAsync(Session session)
+		{
+			await _sessionRepository.Update(session);
+		}
+
+		public async Task DeleteSessionAsync(int id)
+		{
+			await _sessionRepository.Delete(id);
+		}
+	}
+}
