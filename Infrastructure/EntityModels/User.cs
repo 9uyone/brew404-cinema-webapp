82a8d6a0f3aedb0e134311729e6d6dbc6b53522b
Ostap <pamik405@gmail.com>
2025-01-29 19:00:01 +0200

    EnvProperty: fix EnvFullPath; Include ASP.NET Core & Identity

---
+++ Infrastructure/EntityModels/User.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace DataAccess.EntityModels
+{
+	internal class User: IdentityUser
+	{
+
+	}
+}
--- Infrastructure/Property/EnvProperty.cs
+++ Infrastructure/Property/EnvProperty.cs
@@ -3,7 +3,7 @@ namespace BusinessLogic.Property
 {
 	public  static class EnvProperty
 	{
-		public static string EnvFullPath = Path.GetFullPath(Path.Combine(AppContext.BaseDirectory, "..", "..", "..", "..")) + "\\.env";
+		public static string EnvFullPath = Path.Combine(Directory.GetParent(Directory.GetCurrentDirectory())?.FullName ?? "", ".env");
 		public static string DbConnection = "DB_CONNECTION";
 		public static string TmdbApiKey = "TMDB_API_KEY";
 	}
