a9cf4ff68012a5234251a8cf7cc15b2c3aced48a
VladOSSss228 <vladuslavantonyak94@gmail.com>
2025-02-10 23:29:26 +0200

    session filter small changes

--- Infrastructure/EntityModels/Ticket.cs
+++ Infrastructure/EntityModels/Ticket.cs
@@ -7,7 +7,7 @@ namespace DataAccess.EntityModels
 		public int Id { get; set; }
 
 		public int UserId { get; set; }
-		//public User User { get; set; }
+		public User? User { get; set; }
 
 		public int SeatId { get; set; }
 		public Seat? Seat { get; set; }
