5a0799040ff79aa61fd41d680d3a2cdf270475d7
VladOSSss228 <vladuslavantonyak94@gmail.com>
2025-02-15 22:06:54 +0200

    user-tickets

--- WebApp/ViewModels/ProfileViewModel.cs
+++ WebApp/ViewModels/ProfileViewModel.cs
@@ -1,8 +1,11 @@
-﻿namespace WebApp.ViewModels
+﻿using BusinessLogic.DTOs;
+
+namespace WebApp.ViewModels
 {
 	public class ProfileViewModel
 	{
 		public string Name { get; set; } = null!;
+		public List<TicketDTO>? Tickets { get; set; }
 
 	}
 }
