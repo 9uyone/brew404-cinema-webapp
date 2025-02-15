9918bb67aec5c460891f7f86c784b6c145b0191e
Ostap <pamik405@gmail.com>
2025-02-15 03:41:47 +0200

    Fix: show error messages to user

--- WebApp/wwwroot/js/site.js
+++ WebApp/wwwroot/js/site.js
@@ -1,4 +1,17 @@
-﻿$(document).on('submit', 'form[data-ajax="true"]', function (e) {
+﻿function handleAjaxError(response) {
+	console.log(response);
+
+    const data = response.responseJSON;
+    if (data && data.errors !== undefined)
+        alert(Object.values(data.errors).flat().join('\n'));
+    else if (data)
+        alert(Object.values(data).flat().join('\n'));
+    else if (response.responseText !== null)
+        alert(response.responseText);
+    else alert('Сталася помилка');
+}
+
+$(document).on('submit', 'form[data-ajax="true"]', function (e) {
     e.preventDefault();
     $.ajax({
         url: $(this).attr('action'),
@@ -7,20 +20,7 @@
         success: function (response, status, xhr) {
             window.location.reload();
         },
-        error: function (response) {
-            const data = response.responseJSON;
-
-            if (data && data.errors !== undefined)
-                alert(Object.values(data.errors).flat().join('\n'));
-
-            else if (data)
-                alert(Object.values(data).flat().join('\n'));
-
-            else if (response.responseText !== null)
-                alert(response.responseText);
-
-            else alert('Сталася помилка');
-        }
+        error: handleAjaxError
     });
 }); 
 
